79c6a5d4bcbed553c20f1c32620bb086ce8275ab
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2025-01-14 21:57:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -57,7 +57,7 @@ MULTI_FSUB = [int(channel_id) for channel_id in environ.get('AUTH_CHANNEL', '-10
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://subhadeepsamui789:devils2@cluster0.hqos4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://hhhkkkbbb:devils21@cluster0.xuqky.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
 
 # ============================
 # Movie Notification & Update Settings
